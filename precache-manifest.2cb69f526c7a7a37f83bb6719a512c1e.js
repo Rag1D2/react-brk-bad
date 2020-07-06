a6718893406a5e1107c69af6b64d2e40de321c78
Matt Ragone <m.ragone85@gmail.com>
2020-07-06 14:16:02 -0400

    Updates

---
+++ precache-manifest.2cb69f526c7a7a37f83bb6719a512c1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55782f256de8f1a0d9d6e36c635c378e",
+    "url": "/react-brk-bad/index.html"
+  },
+  {
+    "revision": "54d3a4416da23a5db6da",
+    "url": "/react-brk-bad/static/css/main.f4617764.chunk.css"
+  },
+  {
+    "revision": "af207c1e1c35a5a9addd",
+    "url": "/react-brk-bad/static/js/2.160617ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-brk-bad/static/js/2.160617ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d3a4416da23a5db6da",
+    "url": "/react-brk-bad/static/js/main.1c6f6316.chunk.js"
+  },
+  {
+    "revision": "7fa4ca1ea37bdbd43852",
+    "url": "/react-brk-bad/static/js/runtime-main.490b9677.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/react-brk-bad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/react-brk-bad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/react-brk-bad/static/media/spinner.235b9b3e.gif"
+  }
+]);
